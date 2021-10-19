13ef40bc6fe4311f4ff26cbe739a141dbf3894e1
Amruta <amruta.pendse@teqfocus.com>
2021-10-19 12:57:58 +0530

    Alter table

--- djangomodel/models.py
+++ djangomodel/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=120)
-   # description = models.TextField(null=True)
+    description = models.TextField(null=True)
     price = models.DecimalField(max_digits=20, decimal_places=2)
