c03ebf6a98074933b4b2cc65c8af322dea7f3f9d
Prabhat <prabhat.rana828@gmail.com>
2021-10-19 16:16:33 +0530

    alter product

--- djangomodel/models.py
+++ djangomodel/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(null=True)
+    short_description = models.CharField(max_length=120, null=True)
     price = models.DecimalField(max_digits=20, decimal_places=2)
